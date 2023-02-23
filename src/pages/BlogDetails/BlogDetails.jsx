d4ba646d3927d354c3186f8a7c393857785e5c6b
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-23 11:39:49 +0200

    add user interface for each blog and blog page

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -4,21 +4,44 @@ import { useParams } from "react-router-dom"
 
 import styles from "./BlogDetails.module.css"
 
+import Loading from "../Loading/Loading"
+import AuthorInfo from "../../components/AuthorInfo/AuthorInfo"
+
 import * as blogService from "../../services/blogService"
 
 const BlogDetails = (props) => {
   const { id } = useParams()
   const [blog, setBlog] = useState(null)
 
-  useEffect(()=>{
-    const fetchBlog=async()=>{
+  useEffect(() => {
+    const fetchBlog = async () => {
       const data = await blogService.show(id)
       setBlog(data)
     }
     fetchBlog()
-  },[id])
+  }, [id])
 
-  return <main>Details</main>
+  if (!blog) return <Loading />
+
+  return (
+    <main className={styles.container}>
+      <article>
+        <header>
+          <h3>
+            {blog.category.toUpperCase()}
+            <h1>{blog.title}</h1>
+          </h3>
+          <span>
+            <AuthorInfo content={blog} />
+          </span>
+        </header>
+        <p>{blog.text}</p>
+      </article>
+      <section>
+        <h1>Comments</h1>
+      </section>
+    </main>
+  )
 }
 
 export default BlogDetails
